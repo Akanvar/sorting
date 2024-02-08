1528477376ad304560273eb5f3e03905e19e88ec
Mercy Agbasah <agbasahmercy761@gmail.com>
2024-02-08 01:44:49 +0000

    hbnb

--- hbnb/console.py
+++ hbnb/console.py
@@ -1,8 +1,38 @@
 import cmd
+from base_model_1 import BaseModel, storage
 
 
 class HBNBCommand(cmd.Cmd):
     prompt = "(hbnb) "
+
+    def do_create(self, class_name):
+        """Creates a new instance of BaseModel"""
+
+        if not class_name:
+            print("** class name missing **")
+        elif class_name != "BaseModel":
+            print("** class doesn't exist **")
+        else:
+            new_instance = BaseModel()
+            new_instance.save()
+            print(new_instance.id)
+
+    def do_show(self, class_name, class_id):
+        """Prints the string representation of an instance
+         based on the class name and id
+         """
+        if not class_name:
+            print("** class name missing **")
+        elif class_name != "BaseModel":
+            print("** class doesn't exist **")
+        elif not class_id:
+            print("** instance id missing **")
+        storage.reload()
+        
+
+
+
+
 
     def emptyline(self):
         pass
